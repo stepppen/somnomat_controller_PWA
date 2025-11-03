9f830fc9749880252f0a48b0ae70c6c2ee8239f1
09step <stepan.vedunov@zhdk.ch>
2025-11-03 14:11:11 +0100

    restructuring for easier deployment

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,6 +1,6 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
-import dotenv from 'dotenv';
+import dotenv from 'dotenv/lib/main.js';
 
 dotenv.config();
 
--- packages/frontend/nuxt.config.ts
+++ packages/frontend/nuxt.config.ts
@@ -9,7 +9,7 @@ export default defineNuxtConfig({
     public: {
       apiBase: process.env.NUXT_PUBLIC_API_BASE || 'http://localhost:3001'
     }
-  }
+  },
   vite: {
     plugins: [
       tailwindcss(),
