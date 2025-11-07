c9b2250421fabe51d7182c62754e6af1355d0bcd
09step <stepan.vedunov@zhdk.ch>
2025-11-07 12:33:39 +0100

    changed apiBase

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,7 +9,7 @@ export default defineNuxtConfig({
   
   runtimeConfig: {
     public: {
-      apiBase: process.env.NUXT_PUBLIC_API_BASE || 'http://localhost:10000'
+      apiBase: process.env.NUXT_PUBLIC_API_BASE
     }
   },
   vite: {
