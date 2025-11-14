e84bc17b199f1dc5d6a8ea3238629f1a1f537e9f
09step <stepan.vedunov@zhdk.ch>
2025-11-14 14:54:48 +0100

    potential splash screen fix: correct file path in config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -34,23 +34,23 @@ export default defineNuxtConfig({
           type: "image/png",
         },
         {
-          src: 'pwa-64x64.png',
+          src: '/icons/pwa-64x64.png',
           sizes: '64x64',
           type: 'image/png'
         },
         {
-          src: 'pwa-192x192.png',
+          src: '/icons/pwa-192x192.png',
           sizes: '192x192',
           type: 'image/png'
         },
         {
-          src: 'pwa-512x512.png',
+          src: '/icons/pwa-512x512.png',
           sizes: '512x512',
           type: 'image/png',
           purpose: 'any'
         },
         {
-          src: 'maskable-icon-512x512.png',
+          src: '/icons/maskable-icon-512x512.png',
           sizes: '512x512',
           type: 'image/png',
           purpose: 'maskable'
