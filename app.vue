d7a6a046cca2df721891d43f58e5ee3f4cca511c
09step <stepan.vedunov@zhdk.ch>
2025-12-10 13:45:33 +0100

    fix attempt: heading PWA

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="min-h-screen bg-[#F2F2F2] pt-[env(safe-area-inset-top)] pb-[env(safe-area-inset-bottom)]">
     <NuxtLayout>
       <NuxtPage />
     </NuxtLayout>
